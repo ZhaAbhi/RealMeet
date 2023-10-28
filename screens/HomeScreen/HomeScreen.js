8badc30de31557d5c23b296f05667973aff44e21
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-10-28 15:12:10 +0545

    Extracted styles and removed unused imports

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {SafeAreaView, Text, TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 import MapView from 'react-native-maps';
 
 const HomeScreen = () => {
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
       <MapView
-        style={{flex: 1}}
+        style={styles.map}
         initialRegion={{
           latitude: 27.700001,
           longitude: 85.333336,
@@ -14,30 +14,40 @@ const HomeScreen = () => {
           longitudeDelta: 0.0421,
         }}
       />
-      <View
-        style={{
-          position: 'absolute',
-          top: '10%',
-          left: 0,
-          right: 0,
-          padding: 20,
-        }}>
+      <View style={styles.searchContainer}>
         <TextInput
           testID="searchBar"
           placeholder="Find mate around you"
-          style={{
-            backgroundColor: 'white',
-            paddingLeft: 15,
-            padding: 10,
-            borderRadius: 25,
-            color: 'grey',
-            fontSize: 16,
-            fontFamily: 'Roboto-Medium',
-          }}
+          style={styles.searchBar}
         />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  searchContainer: {
+    position: 'absolute',
+    top: '10%',
+    left: 0,
+    right: 0,
+    padding: 20,
+  },
+  searchBar: {
+    backgroundColor: 'white',
+    paddingLeft: 15,
+    padding: 10,
+    borderRadius: 25,
+    color: 'grey',
+    fontSize: 16,
+    fontFamily: 'Roboto-Medium',
+  },
+});
 
 export default HomeScreen;
